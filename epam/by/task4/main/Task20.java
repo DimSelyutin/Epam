e481c6d70ac6be8c091b345cc7b5271ee1cdd18d
Selyutin Dmitriy <102747240+DimSelyutin@users.noreply.github.com>
2022-08-04 19:16:19 +0300

    Update Task20.java

--- epam/by/task4/main/Task20.java
+++ epam/by/task4/main/Task20.java
@@ -8,22 +8,27 @@ public class Task20 {
     public static void main(String[] args) {
         System.out.print("Введите кол-во значений в массиве: ");
         ChekInt ch = new ChekInt();
+        
         int a = ch.chekInt();
         ArrFill arf = new ArrFill();
         int[] massiv = arf.arrays(a);
+        
         System.out.println("Массив значений создан.");
         for (int i : massiv) {
             System.out.print(i+" | ");
         }
+        
         for (int i = 0; i < massiv.length; i++) {
             if (i%2!=0) {
                 massiv[i]=0;
             }
         }
+        
         System.out.println("\nИзмененный массив: ");
         for (int i : massiv) {
             System.out.print(i+" | ");
         }
+        
         System.out.println("\nСдвиг значенией:");
         int count = 0;
         for (int i = 0; i < massiv.length; i++) {
@@ -35,6 +40,7 @@ public class Task20 {
             }
 
         }
+        
         for (int i : massiv) {
             System.out.print(i+" | ");
         }
