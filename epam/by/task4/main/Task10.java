c2a09a1323bc22b73f81a0499f354fb3f5941f1a
Selyutin Dmitriy <102747240+DimSelyutin@users.noreply.github.com>
2022-08-04 19:05:21 +0300

    Update Task10.java

--- epam/by/task4/main/Task10.java
+++ epam/by/task4/main/Task10.java
@@ -7,16 +7,22 @@ public class Task10 {
     public static void main(String[] args) {
         System.out.print("Введите кол-во значений в массиве: ");
         ChekInt ch = new ChekInt();
+        
         int a = ch.chekInt();
         ArrFill arf = new ArrFill();
+        
         int[] massiv = arf.arrays(a);
+        
         System.out.println("Массив значений создан.");
         System.out.print("Введите число больше которого вывести значения: \ni: ");
+        
         int num = ch.chekInt();
+        
         for (int i : massiv) {
             if (i>num) {
                 System.out.println(i);
             }
         }
+        
     }
 }
