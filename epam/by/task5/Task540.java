815790d3c4ca6956ed439bd17f2e6fb098102208
Selyutin Dmitriy <102747240+DimSelyutin@users.noreply.github.com>
2022-08-11 18:57:39 +0300

    Update Task540.java

--- epam/by/task5/Task540.java
+++ epam/by/task5/Task540.java
@@ -18,52 +18,51 @@ public class Task540 {
 		int count=0;
 		for(int i=0,j=arr.length/2;count!=x*x;i--,j++){
 
-		count++;
+			count++;
 
-	    	if(i==-1){
-		i=arr.length-1;
-	    	} else if(j==arr.length){
-		j=0;
-	    	}
+			if(i==-1){
+				i=arr.length-1;
+			} else if(j==arr.length){
+				j=0;
+			}
 
-	    	if(arr[i][j]!=0){
-		i+=2;
-		j-=1;
-	    	} 
-		     
-	    	if(j>=arr.length){
-			j=-1;
-		    }else if(i<0){
-			i=arr.length;
-	    	}
+			if(arr[i][j]!=0){
+				i+=2;
+				j-=1;
+			} 
 
-	    	if(!(i==0 && j==arr.length-1)){
-		         
-                System.out.println(i+" "+j);
-                arr[i][j]=count;
-		    } else {
-		        System.out.println(i+" "+j);
-                arr[i][j]=count;    
-                i=2;
-                j=arr.length-2;
-		    }
-	
-		    if(count==x*x){
-		        count=x*x;
-		    }
-		}
-		
-		//цикл для четного опрядка
-		
-		
-		
-		
-		
-		for(int i = 0; i<arr.length;i++){
-		    for(int j =0;j<arr.length;j++){
-		        System.out.print("\t"+arr[i][j]);
-		    }
-		    System.out.println("\n");
-		}
+			if(j>=arr.length){
+				j=-1;
+			    }else if(i<0){
+				i=arr.length;
+			}
+
+			if(!(i==0 && j==arr.length-1)){
+				System.out.println(i+" "+j);
+				arr[i][j]=count;
+			    } else {
+				System.out.println(i+" "+j);
+				arr[i][j]=count;    
+				i=2;
+				j=arr.length-2;
+			    }
+
+			    if(count==x*x){
+				count=x*x;
+			    }
+			}
+
+			//цикл для четного опрядка
+
+
+
+
+
+			for(int i = 0; i<arr.length;i++){
+			    for(int j =0;j<arr.length;j++){
+				System.out.print("\t"+arr[i][j]);
+			    }
+			    System.out.println("\n");
+			}
 	}
 }
