65b1bcf7d2d5a27750a4198fd29043b4b4fa9245
Selyutin Dmitriy <>
2022-09-08 19:48:14 +0300

    task05

--- by/epam/globTask7/task05/main/Main.java
+++ by/epam/globTask7/task05/main/Main.java
@@ -1,8 +1,17 @@
 package by.epam.globTask7.task05.main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        Counter ct = new Counter();
-        ct.equals(2);
+        Scanner sc = new Scanner(System.in);
+        Counter counter = new Counter();
+        System.out.println("Enter number of start: ");
+        counter.setNumberStart(sc.nextInt());
+        System.out.println("Enter number of end: ");
+        counter.setNumberEnd(sc.nextInt());
+        counter.counter();
+        counter.counterPrint();
+        
     }
 }
