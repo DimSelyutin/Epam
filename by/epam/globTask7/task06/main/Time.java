a12bf236eecdf96e4a0474a376b157485b3a02cf
Selyutin Dmitriy <>
2022-09-08 20:42:07 +0300

    Time

---
+++ by/epam/globTask7/task06/main/Time.java
@@ -0,0 +1,59 @@
+package by.epam.globTask7.task06.main;
+
+
+public class Time {
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+
+    public Time(){
+        hours = 0;
+        minutes = 10;
+        seconds = 0;
+    }
+
+    public void setHours(int _hours){
+        if (_hours<=24 && _hours>=0) {
+            hours = _hours;
+        } else {
+            hours = 0;
+        }
+    }
+    public int getHours(){
+        return hours;
+    }
+
+    public void setMinutes(int _minutes){
+        if (_minutes<=60 && _minutes>=0) {
+            minutes = _minutes;
+        } else {
+            minutes = 0;
+        }
+
+    }
+
+    public int getMinutes(){
+        return minutes;
+    }
+
+    public void setSeconds(int _seconds){
+        if (_seconds<=60 && _seconds>=0) {
+            seconds = _seconds;
+        } else {
+            seconds = 0;
+        }
+    }
+    public int getSeconds(){
+        return seconds;
+    }
+
+    public void changeTime(int hour, int minute, int second){
+        setHours(hour);
+        setMinutes(minute);
+        setSeconds(second);
+    }
+ 
+
+
+}
