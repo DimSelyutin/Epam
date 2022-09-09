549d813f709a85de1659f89c2c62b7968c31d177
Selyutin Dmitriy <>
2022-09-09 19:45:50 +0300

    Customer

--- by/epam/globTask7/task08/main/Agregator.java
+++ by/epam/globTask7/task08/main/Agregator.java
@@ -1,5 +1,37 @@
 package by.epam.globTask7.task08.main;
 
-public class Agregator {
+
+import java.util.Comparator;
+import java.util.List;
+
+
+public class Agregator implements Comparable<Customer>{
+    public Agregator(){
+
+    }
+    
+    
+    public void sortAlph(List<Customer> customer){
+        customer.sort(Comparator.comparing(Customer::getName));
+        for (Customer cus : customer) {
+            cus.print();
+        }
+    }
+
+
+    public void cardSearch(List<Customer> customer, int num1, int num2){
+        for (Customer cus : customer) {
+            if (cus.getCardNumber()>num1 && cus.getCardNumber() < num2) {
+                cus.print();
+            }
+        }
+    }
+
+
+    @Override
+    public int compareTo(Customer o) {
+        
+        return 0;
+    }
     
 }
