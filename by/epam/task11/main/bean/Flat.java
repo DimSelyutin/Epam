6e5ec265060cd8a5a014b618f4e7a232d416e5d7
Selyutin Dmitriy <>
2022-09-12 23:23:49 +0300

    del notuse import

--- by/epam/task11/main/bean/Flat.java
+++ by/epam/task11/main/bean/Flat.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.Objects;
 import java.util.Random;
 
