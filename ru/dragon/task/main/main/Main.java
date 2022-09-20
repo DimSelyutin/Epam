9c4a8445c4f310ddddf6deaecd6191aa180fec20
Selyutin Dmitriy <dima.selyutn.9@mail.ru>
2022-09-20 18:01:04 +0300

    .

---
+++ ru/dragon/task/main/main/Main.java
@@ -0,0 +1,23 @@
+package ru.dragon.task.main.main;
+
+import java.io.Serializable;
+import ru.dragon.task.main.bean.Cave;
+import ru.dragon.task.main.logic.Command;
+import ru.dragon.task.main.logic.GetTreasure;
+import ru.dragon.task.main.view.Present;
+
+
+
+public class Main implements Serializable{
+    public static void main(String[] args) {
+        Cave cv = new Cave(6);
+            
+        Command setTreasures = new GetTreasure(cv);
+
+        Present see = new Present(setTreasures);
+
+        see.getTreasure();
+    }
+
+
+}
