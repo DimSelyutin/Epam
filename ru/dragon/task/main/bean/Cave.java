01eec7abe37a9fc8e94f91680750fbfc11db0ba0
Selyutin Dmitriy <dima.selyutn.9@mail.ru>
2022-09-20 18:02:10 +0300

    .

--- ru/dragon/task/main/bean/Cave.java
+++ ru/dragon/task/main/bean/Cave.java
@@ -24,5 +24,21 @@ public class Cave implements Serializable{
         return treasures;
     }
 
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
 
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return super.hashCode();
+    }
 }
