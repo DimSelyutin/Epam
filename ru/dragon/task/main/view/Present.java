ee71e43184b65580b378c266eafe6b8d0d3eb5b1
Selyutin Dmitriy <dima.selyutn.9@mail.ru>
2022-09-21 21:24:34 +0300

    .

--- ru/dragon/task/main/view/Present.java
+++ ru/dragon/task/main/view/Present.java
@@ -3,16 +3,6 @@ package ru.dragon.task.main.view;
 import ru.dragon.task.main.logic.Command;
 
 public class Present {
-   private Command getTreasureFromCave;
 
-   public Present(Command getTreasureFromCave){
-
-        this.getTreasureFromCave = getTreasureFromCave;
-        
-   }
-
-   public void getTreasure(){
-        getTreasureFromCave.execute();
-   }
 
 }
